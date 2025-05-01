1ddcddcfaa5406e64308b9a26917486c6110afa0
JiPang22 <wlvud6902@gmail.com>
2025-05-02 04:34:50 +0900

    프로젝트 설정 수정 및 기본 출력 기능 추가
    프로젝트 파일에서 불필요한 설정을 제거하고, 디버그 및 릴리스 구성에 대한 PropertySheets를 추가했습니다.
    새로운 소스 파일 `hi.c`와 `main.c`를 포함시켰으며, `hi.c`에는 "hi"를 출력하는 `main_2` 함수가 추가되었습니다.
    `main.c`에서는 `main_2`를 호출하여 출력하도록 변경하였습니다. 주석 처리된 함수 선언은 유지되었습니다.

---
+++ Project-rk4/hi.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main_2() {
+	printf("hi");
+}
---
+++ Project-rk4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main_2();
+
+
+/*
+int main_3();
+int main_4();
+int main_5();
+int main_6();
+int main_7();
+int main_8();
+*/
+
+
+int main() {
+	main_2();
+	return 0;
+}
